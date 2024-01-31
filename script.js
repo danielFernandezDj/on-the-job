9319dfa5128c6ccd2464f846b59a1b0a76d2f3f5
danielFernandezJd <mdanif9728@gmail.com>
2024-01-30 21:34:05 -0800

    Add the CSS code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 /* @@@@@@@@@@@@@@@@@@@@@@@ [ FOOTER STYLING ] @@@@@@@@@@@@@@@@@@@@@@@ */
 var simpleFadeIn = $(".simple-fade-in");
-simpleFadeIn.fadeIn(5000);
+simpleFadeIn.fadeIn(3000);
